e0b3efb0d48c3b1ff019529e171dc21a62b85c54
goncharov.vv <goncharov.vv@esphere.ru>
2023-08-27 21:20:53 +0300

    Commit 01

---
+++ tests/test_web_selenium.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/simple_form_find_task.html")
+# button = browser.find_element(By.ID, "submit") ошибочный id
+button = browser.find_element(By.ID, "submit_button")
+
+# закрываем браузер после всех манипуляций
+browser.quit()
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
